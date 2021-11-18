cca1b28d82cc6af1717a66deee1c11b553ebe51e
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2021-11-18 17:06:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'The Anishinaabeg Creation',
-            image: './images/Anishinaabeg.jpg',
+            image: './images/Muskrat Diver by Shannon Noori.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [-88.34675, 42.56382],
